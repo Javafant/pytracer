22f4ec669ef6202946f16a9cafb44f4eb8b8b651
Moritz Kiefer <moritz.kiefer@gmail.com>
2013-03-02 22:20:10 +0100

    Material parsing

--- materials.py
+++ materials.py
@@ -1,15 +1,26 @@
-import xml.etree.ElementTree as ET
+import color
+
+def parse(mat_node):
+    materials = []
+    for node in mat_node:
+        materials.append(Material(node))
 
 
 class Material:
     ''' it’s a material
     '''
 
-    def __init__(self):
-        pass
-
-    def parse(self, matNode)
-    ''' parses the Xml of material
-        :param matNode: material nodes
-    '''
-        pass
+    def __init__(self, node):
+        for c in node:
+            if c.tag == 'ambient':
+                self.ambient_color = color.parse(c[0])
+                self.ambient_color *= float(c.attrib['factor'].replace(',', '.'))
+            elif c.tag == 'diffuse':
+                self.diffuse_color = color.parse(c[0])
+                self.diffuse_color *= float(c.attrib['factor'].replace(',', '.'))
+            elif c.tag == 'specular':
+                self.specular_color = color.parse(c[0])
+                self.specular_color *= float(c.attrib['factor'].replace(',', '.'))
+            elif c.tag == 'reflection':
+                self.reflection_color = color.parse(c[0])
+                self.reflection_color *= float(c.attrib['factor'].replace(',', '.'))
