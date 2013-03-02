0c96fe210bf019766eb10cfc6285b76fc673a5b2
Baŝto <msnhasser@web.de>
2013-03-02 20:30:44 +0100

    flake8

--- scene.py
+++ scene.py
@@ -23,24 +23,31 @@ class Scene:
         self.camera = camera.parse(scene.findall('camera'))
 
     def send_ray(self, r, recursion_level=4):
+    #does it make sense to set the recursion level per ray?
         ''' sends a ray t
             :param Ray r: starpoint and direction
             :pram int recursion_level: how many light recursion
         '''
         t_min = float('inf')
-        for o in self.objects:#find the closest object
+        for o in self.objects:
+        #find the closest object
             t, tmp_point, tmp_normal = o.intersects(r)
             if t < t_min:
                 t_min = t
                 hit = o
                 point = tmp_point
-                normal = tmp_normal#normalvector
+                normal = tmp_normal
+                #normalvector
 
         if hit:
-            new_color = hit.material.ambient_color * self.ambient_light#a tiny base glow of everything
-            for ls in self.lights:#check all ligths
-                if ls.is_visible_from_point(point, normal, self.objects):#if the light is visibile from the point the ray hit
+            new_color = hit.material.ambient_color * self.ambient_light
+            #a tiny base glow of everything
+            for ls in self.lights:
+            #check all ligths
+                if ls.is_visible_from_point(point, normal, self.objects):
+                #if the light is visibile from the point the ray hit
                     if normal * ls.light_direction(point) > 0:
+                    #if there is still light
                         new_color += color.dot(hit.material.diffuse_color,
                                                ls.get_color(point) *
                                                vector.dot(normal,
@@ -58,14 +65,10 @@ class Scene:
                                       (lr.length * r.direction.length) **
                                       hit.material.phong_specular_exponent)
 
-    def GetMaterialByName(strName)
+    def getMaterialByName(strName)
         '''
             :param str strName: name of the wanted material
             :returns: a material with strName as name or non if not found
             :rtype: Material
         '''
-            for m in self.materials:
-                if m.Name == strName:
-                    return m
-            return non
-        }
+            return self.materials.findall(strName)
